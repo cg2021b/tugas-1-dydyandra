f0984ea587e106397967783fc7ad76675007a53d
Dyandra Paramitha <73653209+dydyandra@users.noreply.github.com>
2021-10-31 12:51:54 +0700

    commit pertama

---
+++ task-4-grafkomb/script.js
@@ -0,0 +1,243 @@
+import * as THREE from './js/three.module.js';
+import {OrbitControls} from '/js/OrbitControls.js';
+import {GLTFLoader} from '/js/GLTFLoader.js';
+import {Reflector} from '/jsm/objects/Reflector.js';
+
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene();
+
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth;
+    sizes.height = window.innerHeight;
+    
+    // Update camera
+    camera.aspect = sizes.width / sizes.height;
+    camera.updateProjectionMatrix();
+    
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 1, 100);
+camera.position.x = -15;
+camera.position.y = 10;
+camera.position.z = 20;
+scene.add(camera);
+
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.autoRotate = true;
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+renderer.shadowMap.enabled = true;
+renderer.render(scene, camera, controls);
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+renderer.gammaOutput = true;
+
+
+
+/**
+ * Panorama 
+ */
+const panorama = new THREE.CubeTextureLoader();
+const textureSun = panorama.load([
+  'sun-texture/px.png',
+  'sun-texture/nx.png',
+  'sun-texture/py.png',
+  'sun-texture/ny.png',
+  'sun-texture/pz.png',
+  'sun-texture/nz.png',
+
+]);
+scene.background = textureSun;
+
+
+/**
+ * Object: Sphere (for Sun)
+ */
+let geometry = new THREE.SphereGeometry(1.54, 10, 10);
+const loader3 = new THREE.TextureLoader();
+loader3.load('img/sunmap.jpg', (sun) => {
+  const material = new THREE.MeshBasicMaterial({
+    map: sun,
+  });
+  const sphere = new THREE.Mesh(geometry, material);
+  sphere.position.x = 20;
+  sphere.position.z = -20;
+  sphere.position.y = 20;
+  scene.add(sphere);
+
+});
+
+/**
+ * Object: Plane
+ */
+
+const loader4 = new THREE.TextureLoader();
+const grass = loader4.load('./img/grasslight-big.jpg');
+grass.wrapS = THREE.RepeatWrapping;
+grass.wrapT = THREE.RepeatWrapping;
+const repeats = 10;
+grass.repeat.set(repeats, repeats);
+
+let grassPlane = new THREE.BoxGeometry(40, 40);
+let grassMaterial = new THREE.MeshLambertMaterial({
+    map:grass
+
+});
+
+
+let plane = new THREE.Mesh(grassPlane,grassMaterial);
+plane.rotation.x = Math.PI / 2;
+plane.position.y = -5.5;
+plane.receiveShadow = true;
+scene.add(plane);
+
+/**
+ * SceneGraph
+ */ 
+function dumpObject(obj, lines = [], isLast = true, prefix = '') {
+    const localPrefix = isLast ? '└─' : '├─';
+    lines.push(`${prefix}${prefix ? localPrefix : ''}${obj.name || '*no-name*'} [${obj.type}]`);
+    const newPrefix = prefix + (isLast ? '  ' : '│ ');
+    const lastNdx = obj.children.length - 1;
+    obj.children.forEach((child, ndx) => {
+      const isLast = ndx === lastNdx;
+      dumpObject(child, lines, isLast, newPrefix);
+    });
+    return lines;
+  }
+
+/**
+ * Object: GLTF
+ */
+
+const loader = new GLTFLoader()
+loader.load('./model/scene.gltf', function(gltf){
+        const root = gltf.scene;
+        root.position.x = 0;
+        root.position.y = -5;
+        scene.add(root);
+        // console.log(dumpObject(root).join('\n'));
+
+        root.traverse(n => { if ( n.isMesh ) {
+          n.castShadow = true; 
+          n.receiveShadow = true;
+        }});
+
+})
+
+/**
+ * Lights
+ */
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(-100,200,100);
+scene.add(pointLight);
+
+const ambientLight = new THREE.AmbientLight(0x000000);
+scene.add(ambientLight);
+
+const solarLight = new THREE.DirectionalLight();
+solarLight.position.set(500, 500, -500);
+solarLight.castShadow = true;
+solarLight.intensity = 2;
+solarLight.shadow.mapSize.width = 1024;
+solarLight.shadow.mapSize.height = 1024;
+solarLight.shadow.camera.near = 250;
+solarLight.shadow.camera.far = 1000;
+
+let intensity=50;
+
+solarLight.shadow.camera.left = -intensity;
+solarLight.shadow.camera.right = intensity;
+solarLight.shadow.camera.top = intensity;
+solarLight.shadow.camera.bottom  = -intensity;
+scene.add(solarLight);
+
+
+/**
+ * Fog
+ */
+
+const near = 20;
+const far = 70;
+const color = 'lightblue';
+scene.fog = new THREE.Fog(color, near, far);
+
+/**
+ * Object: Reflective Sphere
+ */
+const cubeRenderTarget = new THREE.WebGLCubeRenderTarget( 128, { format: THREE.RGBFormat, generateMipmaps: true, minFilter: THREE.LinearMipmapLinearFilter } );
+let sphereCamera = new THREE.CubeCamera(1,500,cubeRenderTarget);
+sphereCamera.position.set(0, 3, 0);
+scene.add(sphereCamera);
+const sphereMirror = new THREE.MeshBasicMaterial({
+  envMap: sphereCamera.renderTarget.texture,
+});
+const sphereGeo = new THREE.SphereGeometry(1.5, 32 , 16);
+const mirrorBall = new THREE.Mesh( sphereGeo, sphereMirror);
+mirrorBall.position.y = 3;
+mirrorBall.position.x = -3;
+scene.add(mirrorBall);
+
+
+/**
+ * Object: Mirror
+ */
+
+let planeMirror = new THREE.PlaneGeometry( 40, 20 );
+const verticalMirror = new Reflector( planeMirror, {
+  clipBias: 0.003,
+  textureWidth: window.innerWidth * window.devicePixelRatio,
+  textureHeight: window.innerHeight * window.devicePixelRatio,
+  color: 0x889999
+} );
+verticalMirror.position.y = 5.5;
+verticalMirror.position.z = - 20;
+scene.add( verticalMirror );
+
+/**
+ * Animate
+ */
+
+const tick = () =>
+{
+
+    // Update Orbital Controls
+    controls.update();
+
+    // Render
+    renderer.render(scene, camera);
+
+    sphereCamera.update(renderer, scene);
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick);
+}
+tick();
